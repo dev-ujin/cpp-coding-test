3fbd3b7cb44d65affd63c8725fbe3c651dcc666c
YouJin Lee <dev.ujin518@gmail.com>
2021-01-31 23:37:22 +0100

    [Add] 2021-01-31 BJ/16283/Farm

---
+++ BJ_16283_Farm/main.cpp
@@ -0,0 +1,38 @@
+/*
+문제 : https://www.acmicpc.net/problem/16283
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a, b, n, w;
+	int sheep, goat;
+	int feed_weight;
+	int total_sheep;
+	int total_goat;
+	int count_solution = 0;
+
+	cin >> a >> b >> n >> w;
+
+	for (int i = 1; i < n; i++) {
+		sheep = i; 
+		goat = n - sheep;
+		feed_weight = a * sheep + b * goat;
+		if (count_solution > 1) {
+			break;
+		}
+		if (feed_weight == w) {
+			total_sheep = sheep;
+			total_goat = goat;
+			count_solution++;
+		}
+	}
+
+	if (count_solution == 1) {
+		cout << total_sheep << " " << total_goat;
+	}
+	else {
+		cout << "-1";
+    }
+}
